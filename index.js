b91a9846ed4258d15e674a4d8a704c01f3c8b809
Lewis Godowski <lewis.godowski@bcgdv.com>
2019-10-21 19:46:30 -0700

    new endpoint?

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello, Alex Oh!'))
+app.get('/', (req, res) => {
+    res.send('Hello, Alex Oh!')
+})
 
 app.listen(PORT, () => {
     console.log(`rexy-server listening on port ${PORT}!`)
 })
 
+app.get('/api', (req, res) => {
+    res.json(200, {msg : 'OK'})
+})
 
 
 
