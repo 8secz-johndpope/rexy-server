b208351c4119b31db1ae1db0af6300bf8f9edcb6
Lewis Godowski <lewis.godowski@bcgdv.com>
2019-10-21 19:16:16 -0700

    added mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
@@ -11,4 +12,14 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}/`);
-  });
+});
+
+var uriString = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/HelloMongoose';
+
+mongoose.connect(uriString, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uriString + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uriString);
+    }
+});
