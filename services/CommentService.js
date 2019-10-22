59c50e99fa16bd67c946d3cfcb41b52e4c8b5726
Lewis Godowski <lewis.godowski@bcgdv.com>
2019-10-22 09:23:31 -0700

    finished comment service

--- services/CommentService.js
+++ services/CommentService.js
@@ -1,9 +1,21 @@
 const Comment = require('../models/Comment.js')
 
 exports.create = (req, res) => {
-    if (!req.body.content) {
+    if (!req.body.text) {
         return res.status(400).send({
-            message: "Comment content cannot be empty."
+            message: "Comment must have text."
+        })
+    }
+
+    if (!req.body.userId) {
+        return res.status(400).send({
+            message: "Comment must have a user."
+        })
+    }
+
+    if (!req.body.listId && !req.body.placeId) {
+        return res.status(400).send({
+            message: "Comment must have either a list or a place."
         })
     }
 
@@ -53,6 +65,74 @@ exports.findOne = (res, req) => {
 
         return res.status(500).send({
             message: "An error occurred while retrieving Comment with id " + req.params.commentId
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    if (!req.body.text) {
+        return res.status(400).send({
+            message: "Comment must have text."
+        })
+    }
+
+    if (!req.body.userId) {
+        return res.status(400).send({
+            message: "Comment must have a user."
+        })
+    }
+
+    if (!req.body.listId && !req.body.placeId) {
+        return res.status(400).send({
+            message: "Comment must have either a list or a place."
+        })
+    }
+
+    Comment.findByIdAndUpdate(req.params.commentId, {
+        listId: req.body.listId,
+        placeId: req.body.placeId,
+        text: req.body.text,
+        userId: req.body.userId
+    }, { new : true })
+    .then(comment => {
+        if (!comment) {
+            return res.status(404).send({
+                message: "Comment not found with id " + req.params.commentId
+            })
+        }
+        res.send(comment)
+    }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Comment not found with id " + req.params.commentId
+            })
+        }
+
+        return res.status(500).send({
+            message: "An error occurred while updating Comment with id " + req.params.commentId
+        })
+    })
+}
+
+exports.delete = (req, res) => {
+    Comment.findByIdAndDelete(req.params.commentId)
+    .then(comment => {
+        if (!comment) {
+            return res.status(404).send({
+                message: "Comment not found with id " + req.params.commentId
+            })
+        }
+        res.send({
+            message: "Successfully deleted Comment with id " + req.params.commentId
+        }).catch(err => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: "Comment not found with id " + req.params.commentId
+                })
+            }
+            return res.status(500).send({
+                message: "An error occurred while deleting Comment with id " + req.params.commentId
+            })
         })
     })
 }
