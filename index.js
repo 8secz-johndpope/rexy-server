58a015d3d14ae3dccdd62a7e43f980f8f3d2869d
Lewis Godowski <lewis.godowski@bcgdv.com>
2019-10-21 19:27:51 -0700

    added express

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const http = require('http');
-const mongoose = require('mongoose');
+const http = require('http')
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('Hello, Alex Oh!'))
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, Alex Oh\n')
-});
+app.listen(port, () => {
+    console.log(`rexy-server listening on port ${port}!`)
+})
 
-server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}/`);
-});
 
-var uriString = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/HelloMongoose';
 
-mongoose.connect(uriString, function (err, res) {
-    if (err) {
-        console.log ('ERROR connecting to: ' + uriString + '. ' + err);
-    } else {
-        console.log ('Succeeded connected to: ' + uriString);
-    }
-});
+
+
+
+
+
+
+
+// var mongoose = require('mongoose')
+
+// const hostname = '127.0.0.1'
+// const PORT = process.env.PORT || 5000
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.end('Hello, Alex Oh\n')
+// })
+
+// server.listen(PORT, () => {
+//     console.log(`Server running on ${PORT}/`)
+// })
+
+// var uriString = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/HelloMongoose';
+
+// mongoose.connect(uriString, function (err, res) {
+//     if (err) {
+//         console.log ('ERROR connecting to: ' + uriString + '. ' + err);
+//     } else {
+//         console.log ('Succeeded connected to: ' + uriString);
+//     }   
+// });
