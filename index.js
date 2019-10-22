261fc70cb19f3e84e3101249711812882ecc948a
Lewis Godowski <lewis.godowski@bcgdv.com>
2019-10-21 19:32:58 -0700

    fixed?

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello, Alex Oh!'))
 
-app.listen(port, () => {
-    console.log(`rexy-server listening on port ${port}!`)
+app.listen(PORT, () => {
+    console.log(`rexy-server listening on port ${PORT}!`)
 })
 
 
