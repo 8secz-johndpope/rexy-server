5d8d46084c72a8d5e05991258e84659235366bba
Lewis Godowski <lewis.godowski@bcgdv.com>
2019-10-23 15:48:46 -0700

    removed improper unique on List

--- models/List.js
+++ models/List.js
@@ -8,7 +8,7 @@ const ListSchema = mongoose.Schema({
     groupName: { type: String },
     isDeleted: { type: Boolean, default: false },
     isPrivate: { type: Boolean, default: false },
-    placeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Place", unique: true }],
+    placeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Place" }],
     title: { type: String, required: true }
 }, {
     timestamps: true
